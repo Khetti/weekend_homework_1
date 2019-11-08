18570b03e5f9b8701d55b137a8a3afa50b68fc6e
Greg Shanks <greg.shanks@gmail.com>
2019-11-08 14:24:09 +0000

    first four tests completed successfully

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,11 @@
-def pet_shop_name(pet_shop_hash)
-  return pet_shop_hash[:name]
+def pet_shop_name(name)
+  return name[:name]
+end
+
+def total_cash(cash)
+  return cash[:admin][:total_cash]
+end
+
+def add_or_remove_cash(total_cash, cash)
+  return total_cash[:admin][:total_cash] += cash
 end
